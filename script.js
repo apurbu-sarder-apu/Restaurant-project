e3f57fd5749f0e81c3aefad9587dac58137f901c
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-07-20 22:35:36 +0600

    menu section

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ window.addEventListener("scroll", function(){
         document.querySelector("header").classList.remove("sticky");
     }
 });
+
+// ------------------------menu tabs------------------------
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+    if(e.target.classList.contains("menu-tab-item")&& !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+});
